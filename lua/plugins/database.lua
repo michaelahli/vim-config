f85dd69658089130ac013b6067b57c1a6a713d02
Michael <michael.ahli99@gmail.com>
2026-05-14 14:51:44 +0700

    reinstall dadbodui
    error unable to open buffer due to mod func is unknown

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -2,59 +2,30 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
-      "kristijanhusak/vim-dadbod",
-      "tpope/vim-dadbod",
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
     cmd = {
       "DBUI",
       "DBUIToggle",
       "DBUIAddConnection",
       "DBUIFindBuffer",
-      "DBSelect",
     },
     init = function()
-      vim.g.db_ui_echo_commands = true
-      vim.g.db_ui_show_help = 1
-      vim.g.db_ui_win_border = "rounded"
-      vim.g.db_ui_save_location = "~/.local/share/db_ui"
-    end,
-    config = function()
-      require("dadbod").setup({
-        default_url = "",
-        default_connector = "mysql",
-        default_timeout = 5000,
-      })
+      vim.g.db_ui_save_location = vim.fn.expand("~/.local/share/db_ui")
 
-      require("dadbod-ui").setup({
-        split_orientation = "horizontal",
-        split_size = 0.4,
-        auto_execute = false,
-        execute_query_on_enter = false,
-        show_icon_column = true,
-        reset_connection_on_save = false,
-        ignore_closing_windows = false,
-        select_in_split = true,
-        picker = "telescope",
-      })
-    end,
-  },
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_show_database_icon = 1
+      vim.g.db_ui_force_echo_notifications = 1
+      vim.g.db_ui_win_position = "left"
+      vim.g.db_ui_winwidth = 40
 
-  {
-    "kristijanhusak/vim-dadbod",
-    ft = "sql",
-    dependencies = {
-      "tpope/vim-dadbod",
+      vim.g.db_ui_auto_execute_table_helpers = 1
+    end,
+    keys = {
+      { "<leader>db", "<cmd>DBUIToggle<cr>", desc = "Toggle DBUI" },
+      { "<leader>df", "<cmd>DBUIFindBuffer<cr>", desc = "Find DB Buffer" },
+      { "<leader>da", "<cmd>DBUIAddConnection<cr>", desc = "Add DB Connection" },
     },
-  },
-
-  {
-    "kristijanhusak/vim-dadbod-completion",
-    ft = { "sql", "mysql", "plsql" },
-    dependencies = {
-      "kristijanhusak/vim-dadbod",
-    },
-    config = function()
-      vim.g.db_ui_completion_use_nvim_cmp = true
-    end,
   },
 }
