bea4e5fd7cbef293b0a938ea36ad77f9688cbf3d
Michael <michael.ahli99@gmail.com>
2026-05-15 12:40:20 +0700

    add theme options
    download theme options all at once

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
-  -- Configure LazyVim to use the default TokyoNight theme
+  -- Theme options (lazy-loaded; aktif saat dipilih via :colorscheme atau <leader>uC)
+  { "folke/tokyonight.nvim", lazy = true, opts = { style = "moon" } },
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, opts = { flavour = "mocha" } },
+  { "rebelot/kanagawa.nvim", lazy = true },
+  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+  { "EdenEast/nightfox.nvim", lazy = true },
+  { "sainnhe/gruvbox-material", lazy = true },
+  { "navarasu/onedark.nvim", lazy = true, opts = { style = "darker" } },
+  { "Mofiqul/dracula.nvim", lazy = true },
+  { "sainnhe/everforest", lazy = true },
+
   {
     "LazyVim/LazyVim",
     opts = {
